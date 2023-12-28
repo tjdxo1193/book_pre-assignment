6f1583b905107efdf4a918b15a69848aadac0a86
황성태 <tjdxo11933@naver.com>
2023-12-28 20:52:42 +0900

    first commit

---
+++ src/main/java/book/PreAssignmentApplication.java
@@ -0,0 +1,13 @@
+package book;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PreAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PreAssignmentApplication.class, args);
+    }
+
+}
---
+++ src/test/java/book/PreAssignmentApplicationTests.java
@@ -0,0 +1,13 @@
+package book;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PreAssignmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
